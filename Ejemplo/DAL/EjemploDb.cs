d8be3d58df468bd61223368106cf125ab08a6fca
seatiel ygnacio rosario <seatiel_2010@hotmail.com>
2017-01-28 14:52:19 -0400

    ejemplo

---
+++ Ejemplo/DAL/EjemploDb.cs
@@ -0,0 +1,19 @@
+﻿using Ejemplo.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+
+namespace Ejemplo.DAL
+{
+    public class EjemploDb : DbContext 
+    {
+        public EjemploDb() : base("ConStr")
+        {
+
+        }
+
+        public DbSet<Estudiantes> Estudiante { get; set; }
+    }
+}
---
+++ Ejemplo/FormularioPrincipal.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace Ejemplo
+{
+    partial class FormularioPrincipal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.NombretextBox = new System.Windows.Forms.TextBox();
+            this.Guardarbutton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(144, 106);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nombre:";
+            // 
+            // NombretextBox
+            // 
+            this.NombretextBox.Location = new System.Drawing.Point(230, 106);
+            this.NombretextBox.Name = "NombretextBox";
+            this.NombretextBox.Size = new System.Drawing.Size(100, 22);
+            this.NombretextBox.TabIndex = 1;
+            // 
+            // Guardarbutton
+            // 
+            this.Guardarbutton.Location = new System.Drawing.Point(255, 197);
+            this.Guardarbutton.Name = "Guardarbutton";
+            this.Guardarbutton.Size = new System.Drawing.Size(84, 34);
+            this.Guardarbutton.TabIndex = 2;
+            this.Guardarbutton.Text = "Guardar";
+            this.Guardarbutton.UseVisualStyleBackColor = true;
+            this.Guardarbutton.Click += new System.EventHandler(this.Guardarbutton_Click);
+            // 
+            // FormularioPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(740, 449);
+            this.Controls.Add(this.Guardarbutton);
+            this.Controls.Add(this.NombretextBox);
+            this.Controls.Add(this.label1);
+            this.Name = "FormularioPrincipal";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox NombretextBox;
+        private System.Windows.Forms.Button Guardarbutton;
+    }
+}
+
